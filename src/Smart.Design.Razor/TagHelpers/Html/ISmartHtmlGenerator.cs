2f4600f52230dcb0e963e2257a9db1ac1f24c7cc
Maxime Poulain <maxime.poulain@smart.coop>
2021-10-27 22:02:12 +0200

    Add BaseSmartFormGroupTagHelper
    BaseSmartFormGroupTagHelper is an abstract whose purpose is to render form groups with control generated programmatically instead of direct html.HtmlContentBuilder.
    This allows implementing common self closed tag helpers.

--- src/Smart.Design.Razor/TagHelpers/Html/ISmartHtmlGenerator.cs
+++ src/Smart.Design.Razor/TagHelpers/Html/ISmartHtmlGenerator.cs
@@ -27,6 +27,38 @@ namespace Smart.Design.Razor.TagHelpers.Html
         TagBuilder GenerateSmartInputText(ViewContext viewContext, string id, string name, string placeholder,
             object? value, ModelExpression @for);
 
+        /// <summary>
+        /// Generate a form group with its label and an empty controls container.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <param name="label">The label associated to the form group.</param>
+        /// <param name="helperText"></param>
+        /// <param name="controls">The content of the controls container.</param>
+        /// <returns>A instance of a form group.</returns>
+        TagBuilder GenerateFormGroup(string id, string name, string label, string helperText, TagBuilder controls);
+
+        /// <summary>
+        /// Generates a form group &lt;div&gt;.
+        /// The combination of a form label and an element field are usually wrapped inside this element.
+        /// The consistent markup pattern makes it easy to manipulate grouped form elements.
+        /// </summary>
+        /// <returns> An instance of the &lt;div&gt;</returns>
+        TagBuilder GenerateFormGroup();
+
+        /// <summary>
+        /// Generate a &lt;label&gt; element.
+        /// </summary>
+        /// <param name="label">The value of the label.</param>
+        /// <param name="labelFor"></param>
+        /// <returns>A instance of the &lt;label&gt;</returns>
+        TagBuilder GenerateFormGroupLabel(string label, string labelFor);
+
+        /// <summary>
+        /// Generate a &lt;div&gt; in which are grouped element of a form group.
+        /// </summary>
+        /// <returns> An instance of the &lt;div&gt;</returns>
+        TagBuilder GenerateFormGroupControlsContainer();
 
         /// <summary>
         /// Generate a &lt;div&gt; that represents a panel and is compliant with smart design.
--- src/Smart.Design.Razor/TagHelpers/Html/SmartHtmlGenerator.cs
+++ src/Smart.Design.Razor/TagHelpers/Html/SmartHtmlGenerator.cs
@@ -67,6 +67,81 @@ namespace Smart.Design.Razor.TagHelpers.Html
             return inputTextTagBuilder;
         }
 
+
+        /// <inheritdoc />
+        public TagBuilder GenerateFormGroup(string id, string name, string label, string helperText, TagBuilder controls)
+        {
+            var formGroup = GenerateFormGroup();
+
+            // We allow whitespace in case the developer wants actually to generate the <label> element.
+            if (!string.IsNullOrEmpty(label))
+            {
+                var formGroupLabel = GenerateFormGroupLabel(label, id);
+                formGroup.InnerHtml.AppendHtml(formGroupLabel);
+            }
+
+            // Generate the div holding controls regardless it has content or not.
+            var formGroupControlsContainer = GenerateFormGroupControlsContainer();
+            if (controls is not null)
+            {
+                formGroupControlsContainer.InnerHtml.AppendHtml(controls);
+            }
+
+            if (!string.IsNullOrEmpty(helperText))
+            {
+                var helperTextTagBuilder = GenerateFormGroupHelperText(helperText);
+                formGroupControlsContainer.InnerHtml.AppendHtml(helperTextTagBuilder);
+            }
+
+            formGroup.InnerHtml.AppendHtml(formGroupControlsContainer);
+            return formGroup;
+        }
+
+        /// <inheritdoc />
+        public virtual TagBuilder GenerateFormGroup()
+        {
+            var builder = new TagBuilder("div");
+            builder.AddCssClass("o-form-group");
+
+            return builder;
+        }
+
+        /// <inheritdoc />
+        public TagBuilder GenerateFormGroupLabel(string label, string labelFor)
+        {
+            var labelTagBuilder = new TagBuilder("label");
+            labelTagBuilder.AddCssClass("o-form-group__label");
+            if (!string.IsNullOrWhiteSpace(label))
+            {
+                labelTagBuilder.InnerHtml.Append(label);
+            }
+
+            if (!string.IsNullOrWhiteSpace(labelFor))
+            {
+                labelTagBuilder.Attributes.Add("for", labelFor);
+            }
+
+            return labelTagBuilder;
+        }
+
+        /// <inheritdoc />
+        public virtual TagBuilder GenerateFormGroupControlsContainer()
+        {
+            var formGroupDiv = new TagBuilder("div");
+            formGroupDiv.AddCssClass("o-form-group__controls");
+
+            return formGroupDiv;
+        }
+
+        public TagBuilder GenerateFormGroupHelperText(string helperText)
+        {
+            var tagBuilder = new TagBuilder("p");
+            tagBuilder.AddCssClass("c-form-help-text");
+            tagBuilder.InnerHtml.SetContent(helperText);
+
+            return tagBuilder;
+        }
+
         /// <inheritdoc />
         public virtual TagBuilder GenerateSmartPanel(string header, IHtmlContent content)
         {
