c7ed0e7b01f9cba5b7e71d823b2a8b8f247f5907
Maxime Poulain <maxime.poulain@smart.coop>
2021-10-27 21:26:26 +0200

    Add BaseTagHelper class and the SmartHtmlGenerator
    BaseTagHelper class is a simple abstract class inheriting from TagHelper, it exposes the possibility to manipulate id and name attributes.
    SmartHtmlGenerator is a html generator whose purpose is to serve smart design tag helper implementations.

---
+++ src/Smart.Design.Razor/TagHelpers/Html/ISmartHtmlGenerator.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+#nullable enable
+namespace Smart.Design.Razor.TagHelpers.Html
+{
+    /// <summary>
+    /// Contract for a service supporting <see cref="IHtmlHelper"/> and <c>ITagHelper</c> implementations compliant with
+    /// <see href="https://design.smart.coop">smart design</see>.
+    /// </summary>
+    public interface ISmartHtmlGenerator
+    {
+        /// <summary>
+    }
+#nullable disable
+}
---
+++ src/Smart.Design.Razor/TagHelpers/Html/SmartHtmlGenerator.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using CaseExtensions;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+namespace Smart.Design.Razor.TagHelpers.Html
+{
+    public class SmartHtmlGenerator : ISmartHtmlGenerator
+    {
+    }
+}
