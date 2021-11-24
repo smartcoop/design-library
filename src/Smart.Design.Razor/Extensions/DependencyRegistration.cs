0eaec5684781fa8388217f6a3d51c6e201b293b5
Maxime Poulain <maxime.poulain@smart.coop>
2021-11-24 18:03:18 +0100

    Add support dependency registration with Microsoft container

---
+++ src/Smart.Design.Razor/Extensions/DependencyRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Smart.Design.Razor.TagHelpers.Html;
+using Smart.Design.Razor.TagHelpers.IconList;
+
+namespace Smart.Design.Razor.Extensions
+{
+    public static class DependencyRegistration
+    {
+        public static IServiceCollection AddSmartDesign(this IServiceCollection services)
+        {
+            return services
+                .AddTransient<ISmartHtmlGenerator, SmartHtmlGenerator>()
+                .AddTransient<ISmartIconListHtmlGenerator, SmartIconListHtmlGenerator>();
+        }
+    }
+}
