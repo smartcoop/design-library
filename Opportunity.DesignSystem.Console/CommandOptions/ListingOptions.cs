8df5ff1aaea5e4c0a031ccf4dc62106b1afb1e9a
Victor van Duijnen <victor.van.duijnen@smart.coop>
2021-11-25 15:32:54 +0100

    feat: init unit tests

--- Opportunity.DesignSystem.Console/CommandOptions/ListingOptions.cs
+++ Opportunity.DesignSystem.Console/CommandOptions/ListingOptions.cs
@@ -3,7 +3,7 @@ using CommandLine;
 namespace Opportunity.DesignSystem.Console.Options
 {
     [Verb("list", HelpText = "Lists design elements")]
-    public class ListOptions
+    public class ListingOptions
     {
         [Option('c', "category", Required = false, HelpText = "Category of design Elements")]
         public string DesignElementCategory { get; set; }
