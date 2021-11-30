924eb39830893020c4a42490bc335455d2579b40
Victor van Duijnen <victor.van.duijnen@smart.coop>
2021-11-30 16:44:38 +0100

    Add Smart.CFA.Pages to solution
    Template for the client-facing CFA project

---
+++ src/Smart.CFA.Pages/Program.cs
@@ -0,0 +1,35 @@
+
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services
+    .AddRazorPages();
+
+
+var app = builder.Build();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
---
+++ src/Smart.CFA.Pages/README.md
@@ -0,0 +1,3 @@
+#Client CFA
+
+Client-facing Razor Page web application in charge of rendering the the CFA views
