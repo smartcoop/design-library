b7e0cb1fead5ffe4a84667b578d54fa9ff7536ab
Maxime Poulain <maxime.poulain@smart.coop>
2021-12-03 18:00:46 +0100

    Add implementation to generate Smart design textarea
    See documentation;: https://design.smart.coop/development/docs/c-textarea.html.

---
+++ src/Smart.Design.Razor/TagHelpers/Elements/Textarea/TextAreaSize.cs
@@ -0,0 +1,12 @@
+namespace Smart.Design.Razor.TagHelpers.Elements.Textarea;
+
+/// <summary>
+/// Valid size of a Smart design textarea.
+/// </summary>
+public enum TextAreaSize
+{
+    Unspecified,
+    Small,
+    Medium,
+    Large
+}
---
+++ src/Smart.Design.Razor/TagHelpers/Elements/Textarea/TextareaHtmlGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Smart.Design.Razor.TagHelpers.Base;
+
+namespace Smart.Design.Razor.TagHelpers.Elements.Textarea;
+
+public class TextareaHtmlGenerator : BaseHtmlGenerator, ITextareaHtmlGenerator
+{
+
+    /// <inheritdoc />
+    public TagBuilder GenerateSmartTextArea(string? id, string? name, int? rows, TextAreaSize textareaSize, ModelExpression? @for)
+    {
+        var textAreaTagBuilder = new TagBuilder("textarea");
+        textAreaTagBuilder.AddCssClass("c-textarea");
+
+        if (!string.IsNullOrWhiteSpace(id))
+        {
+            textAreaTagBuilder.Attributes.Add("id", id);
+        }
+
+        AddNameAttribute(textAreaTagBuilder, @for, name);
+
+        if (rows.HasValue)
+        {
+            textAreaTagBuilder.Attributes.Add("rows", rows.Value.ToString(CultureInfo.InvariantCulture));
+        }
+
+        if (textareaSize != TextAreaSize.Unspecified)
+        {
+            textAreaTagBuilder.AddCssClass($"c-input--h-{textareaSize.ToString().ToLowerInvariant()}");
+        }
+
+        if (@for?.Model is string content && !string.IsNullOrWhiteSpace(content))
+        {
+            textAreaTagBuilder.InnerHtml.SetHtmlContent(content);
+        }
+
+        return textAreaTagBuilder;
+    }
+}
