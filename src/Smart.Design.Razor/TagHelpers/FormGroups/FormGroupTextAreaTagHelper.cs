1b1f53e7a4d9c9223d2f484e391d3f89847093a9
Maxime Poulain <maxime.poulain@smart.coop>
2021-12-03 18:15:48 +0100

    Add a self closed tag helper rendering form group textarea
    This class inherits from  BaseSmartFormGroupTagHelper.

---
+++ src/Smart.Design.Razor/TagHelpers/FormGroups/FormGroupTextAreaTagHelper.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Smart.Design.Razor.TagHelpers.Base;
+using Smart.Design.Razor.TagHelpers.Elements.Textarea;
+
+namespace Smart.Design.Razor.TagHelpers.FormGroups;
+
+/// <summary>
+/// <see cref="BaseSmartFormGroupTagHelper"/> implementation with a <see cref="SmartTextareaTagHelper" />.
+/// </summary>
+[HtmlTargetElement(Constants.TagNames.FormGroupTextArea)]
+public class FormGroupTextAreaTagHelper : BaseSmartFormGroupTagHelper
+{
+    private readonly ITextareaHtmlGenerator _textareaHtmlGenerator;
+    private const string RowsAttributeName = "rows";
+    private const string SizeAttributeName = "size";
+
+    [HtmlAttributeName(SizeAttributeName)]
+    public TextAreaSize TextareaSize { get; set; }
+
+    /// <summary>
+    /// Number of textarea rows.
+    /// </summary>
+    [HtmlAttributeName(RowsAttributeName)]
+    public int? Rows { get; set; }
+
+    public FormGroupTextAreaTagHelper(IFormGroupHtmlGenerator htmlGenerator, ITextareaHtmlGenerator textareaHtmlGenerator) : base(htmlGenerator)
+    {
+        _textareaHtmlGenerator = textareaHtmlGenerator;
+    }
+
+    public override TagBuilder GenerateFormGroupControl()
+    {
+        return _textareaHtmlGenerator.GenerateSmartTextArea(Id, Name, Rows, TextareaSize, For);
+    }
+}
