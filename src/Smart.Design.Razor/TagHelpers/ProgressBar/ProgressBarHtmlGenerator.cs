fec6e3afeed2331637395b794afd9c8f0b3155f2
Maxime Poulain <maxime.poulain@smart.coop>
2021-12-03 20:56:53 +0100

    Add implementation of the Smart design progress bar
    See documentation at https://design.smart.coop/development/docs/c-progress-bar.html.

---
+++ src/Smart.Design.Razor/TagHelpers/ProgressBar/ProgressBarHtmlGenerator.cs
@@ -0,0 +1,22 @@
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Smart.Design.Razor.TagHelpers.ProgressBar;
+
+public class ProgressBarHtmlGenerator : IProgressBarHtmlGenerator
+{
+    /// <inheritdoc />
+    public TagBuilder GenerateProgressBar(int progress = 0)
+    {
+        var progressDecimal = (progress / 100d).ToString("0.##", CultureInfo.InvariantCulture);
+        var progressBar = new TagBuilder("div");
+        progressBar.AddCssClass("c-progress-bar");
+        progressBar.Attributes["style"] = $"--value: {progressDecimal}";
+        if (progress >= 100)
+        {
+            progressBar.AddCssClass("c-progress-bar--success");
+        }
+
+        return progressBar;
+    }
+}
