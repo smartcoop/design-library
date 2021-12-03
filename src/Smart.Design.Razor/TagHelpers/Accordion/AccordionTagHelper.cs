19ddc43fb519602d3cb169247a3e308f3cd8678c
Maxime Poulain <maxime.poulain@smart.coop>
2021-12-03 20:59:35 +0100

    Add Smart design accordion
    Documentation available at https://design.smart.coop/development/docs/c-accordion.html.

---
+++ src/Smart.Design.Razor/TagHelpers/Accordion/AccordionTagHelper.cs
@@ -0,0 +1,90 @@
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Smart.Design.Razor.TagHelpers.Base;
+using Smart.Design.Razor.TagHelpers.Constants;
+using Smart.Design.Razor.TagHelpers.Icon;
+
+namespace Smart.Design.Razor.TagHelpers.Accordion;
+
+/// <summary>
+///<see cref="ITagHelper" /> implementation of the Smart design accordion.
+///Documentation is available <see href="https://design.smart.coop/development/docs/c-accordion.html">here</see>.
+///<para>
+///<term>Remark</term>
+///Inherits from <see cref="BaseTagHelper" />
+///</para>
+/// </summary>
+[HtmlTargetElement(TagNames.AccordionTagName, TagStructure = TagStructure.NormalOrSelfClosing)]
+public class AccordionTagHelper : TagHelper
+{
+    private readonly IIconHtmlGenerator _iconHtmlGenerator;
+    private const string TitleAttributeName = "title";
+
+    public AccordionTagHelper(IIconHtmlGenerator iconHtmlGenerator)
+    {
+        _iconHtmlGenerator = iconHtmlGenerator;
+    }
+
+    [HtmlAttributeName(TitleAttributeName)]
+    public string? Title { get; set; }
+
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        output.Attributes.Clear();
+        output.TagName = "div";
+        output.TagMode = TagMode.StartTagAndEndTag;
+
+        var content = (await output.GetChildContentAsync()).GetContent();
+
+        output.AddClass("c-accordion", HtmlEncoder.Default);
+
+        var accordion = new TagBuilder("div");
+        accordion.AddCssClass("c-accordion__item");
+
+        var accordionHeader = new TagBuilder("div");
+        accordionHeader.AddCssClass("c-accordion__item-header");
+
+        var toolbarContainer = new TagBuilder("div");
+        toolbarContainer.AddCssClass("c-toolbar");
+
+        var toolbarParent = new TagBuilder("div");
+        toolbarParent.AddCssClass("c-toolbar__left");
+
+        var toolbarItem = new TagBuilder("div");
+        toolbarItem.AddCssClass("c-toolbar__item");
+
+        var toolbarButton = new TagBuilder("button");
+        toolbarButton.AddCssClass("c-button c-button--borderless c-button--icon");
+        toolbarButton.Attributes.Add("type", "button");
+
+        var toolbarButtonContent = new TagBuilder("span");
+        toolbarButtonContent.AddCssClass("c-button__content");
+
+        var trailingIcon = await _iconHtmlGenerator.GenerateIconAsync(Image.ChevronRight);
+        toolbarButtonContent.InnerHtml.AppendHtml(trailingIcon);
+
+        var accordionTitle = new TagBuilder("span");
+        accordionTitle.AddCssClass("c-accordion__item-title");
+
+        if (Title is not null)
+            accordionTitle.InnerHtml.Append(Title);
+
+        var accordionContent = new TagBuilder("div");
+        accordionContent.AddCssClass("c-accordion__item-content");
+        accordionContent.InnerHtml.AppendHtml(content);
+
+        toolbarButton.InnerHtml.AppendHtml(toolbarButtonContent);
+        toolbarItem.InnerHtml.AppendHtml(toolbarButton);
+        toolbarItem.InnerHtml.AppendHtml(accordionTitle);
+        toolbarParent.InnerHtml.AppendHtml(toolbarItem);
+        toolbarContainer.InnerHtml.AppendHtml(toolbarParent);
+        accordionHeader.InnerHtml.AppendHtml(toolbarContainer);
+        accordion.InnerHtml.AppendHtml(accordionHeader);
+        accordion.InnerHtml.AppendHtml(accordionContent);
+
+        output.Content.SetHtmlContent(accordion);
+    }
+}
