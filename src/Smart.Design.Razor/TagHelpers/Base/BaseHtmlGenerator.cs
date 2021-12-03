f36353e937cb9ff6cd6b3fbb9a2bb63b45db9258
Maxime Poulain <maxime.poulain@smart.coop>
2021-12-03 18:00:46 +0100

    Add implementation to generate Smart design textarea
    See documentation;: https://design.smart.coop/development/docs/c-textarea.html.

--- src/Smart.Design.Razor/TagHelpers/Base/BaseHtmlGenerator.cs
+++ src/Smart.Design.Razor/TagHelpers/Base/BaseHtmlGenerator.cs
@@ -1,9 +1,28 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace Smart.Design.Razor.TagHelpers.Base;
 
 public class BaseHtmlGenerator
 {
+
+    /// <summary>
+    /// Sets <see cref="TagBuilder" />'s name depending on the value of <paramref name="modelExpression" /> and <paramref name="name"/>.
+    /// If both <paramref name="modelExpression"/> <see cref="ModelExpression.Name"/> and <paramref name="name"/> are empty or null, the attribute is not set.
+    /// <see cref="ModelExpression.Name"/> of <paramref name="modelExpression"/> property has precedence over <paramref name="name" />.
+    /// </summary>
+    /// <param name="tagBuilder">The <see cref="TagBuilder"/> to which attribute name should be set.</param>
+    /// <param name="modelExpression">A <see cref="ModelExpression" /> that can be associated to the <paramref name="tagBuilder" /></param>
+    /// <param name="name">Value of the name attribute if specified.</param>
+    protected void AddNameAttribute(TagBuilder tagBuilder, ModelExpression? modelExpression, string name)
+    {
+        var attributeName = GetAttributeName(name, modelExpression);
+        if (!string.IsNullOrWhiteSpace(attributeName))
+        {
+            tagBuilder.Attributes["name"] = attributeName;
+        }
+    }
+
     protected string? GetAttributeName(string? name, ModelExpression? modelExpression)
     {
         return !string.IsNullOrWhiteSpace(modelExpression?.Name) ? modelExpression.Name : name;
---
+++ src/Smart.Design.Razor/TagHelpers/Elements/Textarea/ITextareaHtmlGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Smart.Design.Razor.TagHelpers.Elements.Textarea;
+
+public interface ITextareaHtmlGenerator
+{
+    /// <summary>
+    /// Generate a &lt;textarea&gt; that is compliant with Smart design.
+    /// </summary>
+    /// <param name="id">The id of the &lt;textarea&gt;</param>
+    /// <param name="name">The name of the &lt;textarea&gt;</param>
+    /// <param name="rows">The number of rows of the &lt;textarea&gt;. This parameter is optional.</param>
+    /// <param name="textareaSize"></param>
+    /// <param name="for"></param>
+    /// <returns>An instance of the &lt;textarea&gt;</returns>
+    TagBuilder GenerateSmartTextArea(string? id, string? name, int? rows, TextAreaSize textareaSize, ModelExpression? @for);
+}
