d2db6264bcf4fca312de005d358680d6082d6499
Maxime Poulain <maxime.poulain@smart.coop>
2021-12-03 17:33:28 +0100

    Add implementation of the Smart design button toolbar
    https://design.smart.coop/development/docs/c-button-toolbar.html.

---
+++ src/Smart.Design.Razor/TagHelpers/ButtonToolbar/ButtonToolbarHtmlGenerator.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Smart.Design.Razor.TagHelpers.ButtonToolbar;
+
+public class ButtonToolbarHtmlGenerator : IButtonToolbarHtmlGenerator
+{
+
+    /// <inheritdoc />
+    public TagBuilder GenerateSmartButtonToolbar(ButtonToolbarLayout layout, bool isCompact)
+    {
+        var toolbar = new TagBuilder("div");
+        toolbar.AddCssClass("c-button-toolbar");
+
+        if (layout == ButtonToolbarLayout.Vertical)
+        {
+            toolbar.AddCssClass("c-button-toolbar--vertical");
+        }
+
+        if (isCompact)
+        {
+            toolbar.AddCssClass("c-button-toolbar--compact");
+        }
+
+        return toolbar;
+    }
+}
