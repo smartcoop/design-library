ad39a80950e45fd876a8fbec0504b346e970e484
Maxime Poulain <maxime.poulain@smart.coop>
2021-12-03 18:23:33 +0100

    Add implementation of the Smart design checkbox
    See documentation: https://design.smart.coop/development/docs/c-checkbox.html.

---
+++ src/Smart.Design.Razor/TagHelpers/Elements/Checkbox/CheckboxHtmlGenerator.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Smart.Design.Razor.TagHelpers.Elements.Checkbox;
+
+public class CheckboxHtmlGenerator : ICheckboxHtmlGenerator
+{
+    /// <inheritdoc />
+    public TagBuilder GenerateCheckbox(string? id, string? name, string? value, string? label, bool disabled, bool @checked, ModelExpression? @for)
+    {
+        // Root HTML element of the component is a div containing a label and an input.
+        var rootDiv = new TagBuilder("div");
+        rootDiv.AddCssClass("c-checkbox");
+
+        // HTML label contains the checkbox.
+        var labelDiv = new TagBuilder("label");
+        var checkbox = new TagBuilder("input")
+        {
+            TagRenderMode = TagRenderMode.SelfClosing,
+            Attributes =
+            {
+                { "type" , "checkbox" },
+                { "value", "true" }
+            }
+        };
+
+        // Sets the id if defined.
+        if (!string.IsNullOrWhiteSpace(id))
+        {
+            checkbox.Attributes["id"] = id;
+        }
+
+        // Determines the name attribute. @for has precedence over the name parameter.
+        if (!string.IsNullOrWhiteSpace(@for?.Name))
+        {
+            checkbox.Attributes["name"] = @for.Name;
+
+            // If the associated model is a collection, the checkbox value should have been hardcoded in the HTML.
+            if (!string.IsNullOrWhiteSpace(value) &&
+                typeof(IEnumerable).IsAssignableFrom(@for.ModelExplorer.ModelType))
+                checkbox.Attributes["value"] = value;
+        }
+        else if (!string.IsNullOrWhiteSpace(name))
+        {
+            checkbox.Attributes["name"] = name;
+        }
+
+        if (disabled)
+        {
+            checkbox.Attributes["disabled"] = "disabled";
+        }
+
+        // Checks the checkbox if the associated expression value is true.
+        // Fallback to checked variable if the model is null.
+        if (@for?.Model is true || @checked)
+        {
+            checkbox.Attributes["checked"] = "checked";
+        }
+
+        // The <label> element has two children: the checkbox followed by the label.
+        labelDiv.InnerHtml.SetHtmlContent(checkbox);
+        if (!string.IsNullOrWhiteSpace(label))
+            labelDiv.InnerHtml.Append(label);
+
+        rootDiv.InnerHtml.SetHtmlContent(labelDiv);
+
+        return rootDiv;
+    }
+}
