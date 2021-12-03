4da53293c7c892f868f3e6687c42b2b01d10ed55
Maxime Poulain <maxime.poulain@smart.coop>
2021-12-03 20:04:59 +0100

    Add implementation of the Smart design loader
    Documentation available at https://design.smart.coop/development/docs/c-loader.html.

--- src/Smart.Design.Razor/TagHelpers/Constants/TagNames.cs
+++ src/Smart.Design.Razor/TagHelpers/Constants/TagNames.cs
@@ -37,6 +37,8 @@ public static class TagNames
 
     public const string InputTimeTagName = "smart-input-time";
 
+    public const string LoaderTagName = "smart-loader";
+
     public const string PanelTagName = "smart-panel";
 
     public const string RadioButtonTagName = "smart-radio";
