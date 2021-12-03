a83f9ca2b6134a3670895ade6773a2500b98f2b1
Maxime Poulain <maxime.poulain@smart.coop>
2021-12-03 21:06:00 +0100

    Add Smart design badge implementation
    Documentation is available at https://design.smart.coop/development/docs/c-badge.html.

---
+++ src/Smart.Design.Razor/TagHelpers/Badge/IBadgeHtmlGenerator.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Smart.Design.Razor.TagHelpers.Icon;
+
+namespace Smart.Design.Razor.TagHelpers.Badge;
+
+public interface IBadgeHtmlGenerator
+{
+    /// <summary>
+    /// Generates a Smart design badge.
+    /// </summary>
+    /// <param name="style">Style to applied to the badge.</param>
+    /// <param name="icon">Icon to use, will override the <paramref name="style" /> default one.</param>
+    /// <param name="size">Size of the badge.</param>
+    /// <param name="title">Title of the badge if set.</param>
+    /// <param name="subline">Subline of the badge if set.</param>
+    /// <returns>An instance of a Smart design badge.</returns>
+    public TagBuilder GenerateSmartBadge(BadgeStyle style, Image icon, BadgeSize size, string? title = default, string? subline = null);
+}
