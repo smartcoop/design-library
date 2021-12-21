270dc80f72ba32b87589d560d6cb2bfd361b18cd
Victor van Duijnen <victor.van.duijnen@smart.coop>
2021-12-21 11:58:42 +0100

    Add a CLI utility to generate html from .cshtml templates
    The CLI is splitted into two projects: a console application and a RCL that holds all cshtml templates.
    The reason is that the engine requires to have cshtml file inside a razor class library to work.

---
+++ src/HtmlGenerator/Smart.Design.HtmlGenerator/Managers/IComponentDiscoverer.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+
+namespace Smart.Design.HtmlGenerator.Managers;
+
+public interface IComponentDiscoverer
+{
+    /// <summary>
+    /// Returns a collection of the Smart design components implemented with Tag Helpers in Smart.Design.Razor library.
+    /// </summary>
+    /// <returns>A list of Tag Helpers rendering Smart design components</returns>
+    List<string> GetAllTagHelperNames();
+}
