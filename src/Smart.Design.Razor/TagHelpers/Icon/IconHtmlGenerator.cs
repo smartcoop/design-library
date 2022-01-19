7940aa6c39b3347281ade6388a8226de3a09f02a
Maxime Poulain <maxime.poulain@smart.coop>
2022-01-19 11:40:04 +0100

    Switch the svg icons's build action from Resource to Embedded Resource
    When the library is used as a package the svg files are not copied to the output directory therefore resolving them results in an error.
    Furthermore using EmbeddedResource is way faster then reading from a storage since former are located in the memory.

--- src/Smart.Design.Razor/TagHelpers/Icon/IconHtmlGenerator.cs
+++ src/Smart.Design.Razor/TagHelpers/Icon/IconHtmlGenerator.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using CaseExtensions;
@@ -8,10 +9,6 @@ namespace Smart.Design.Razor.TagHelpers.Icon;
 
 public class IconHtmlGenerator : IIconHtmlGenerator
 {
-    private static string? s_baseIconPath;
-    private static string BaseIconPath => s_baseIconPath ??=
-        $"{Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)}/wwwroot/icons";
-
     /// <inheritdoc />
     public TagBuilder GenerateIcon(Image image)
     {
@@ -19,7 +16,9 @@ public class IconHtmlGenerator : IIconHtmlGenerator
         iconDiv.AddCssClass($"o-svg-icon o-svg-icon-{image.ToString().ToKebabCase()}");
         if (image != Image.None)
         {
-            var svg = File.ReadAllText($"{BaseIconPath}/{image.ToString().ToKebabCase()}.svg");
+            var svg = RetrieveSvgFromEmbeddedResources(image);
+
+            // IHtmlContentBuilder.AppendHtml does nothing if the argument is null or empty.
             iconDiv.InnerHtml.AppendHtml(svg);
         }
 
@@ -33,10 +32,48 @@ public class IconHtmlGenerator : IIconHtmlGenerator
         iconDiv.AddCssClass($"o-svg-icon o-svg-icon-{image.ToString().ToKebabCase()}");
         if (image != Image.None)
         {
-            var svg = await File.ReadAllTextAsync($"{BaseIconPath}/{image.ToString().ToKebabCase()}.svg");
+            var svg = await RetrieveSvgFromEmbeddedResourcesAsync(image);
+
+            // IHtmlContentBuilder.AppendHtml does nothing if the argument is null or empty.
             iconDiv.InnerHtml.AppendHtml(svg);
         }
 
         return iconDiv;
+    }
+
+    private string? RetrieveSvgFromEmbeddedResources(Image image)
+    {
+        var resourceStream = GetEmbeddedResourceStream(image);
+        if (resourceStream is null)
+            return null;
+
+        using var streamReader = new StreamReader(resourceStream);
+        return streamReader.ReadToEnd();
+    }
+
+    private async Task<string?> RetrieveSvgFromEmbeddedResourcesAsync(Image image)
+    {
+        var resourceStream = GetEmbeddedResourceStream(image);
+        if (resourceStream is null)
+            return null;
+
+        using var streamReader = new StreamReader(resourceStream);
+        return await streamReader.ReadToEndAsync();
+    }
+
+    private Stream? GetEmbeddedResourceStream(Image image)
+    {
+        if (image == Image.None)
+        {
+            return null;
+        }
+
+        var currentAssembly = Assembly.GetExecutingAssembly();
+        var resourceFileName = currentAssembly.GetManifestResourceNames().FirstOrDefault(resourceName => resourceName.Contains(image.ToString().ToKebabCase()));
+
+        if (string.IsNullOrWhiteSpace(resourceFileName))
+            return null;
+
+        return currentAssembly.GetManifestResourceStream(resourceFileName);
     }
 }
