04fd6cd4f72a07fa303549ca9b74dad4a3e759f3
Maxime Poulain <maxime.poulain@smart.coop>
2022-02-16 20:22:07 +0100

    Add implementation of the Smart Design Key Value List
    See documentation at https://design.smart.coop/development/docs/c-key-value.html.

---
+++ src/Smart.Design.Razor/TagHelpers/KeyValueList/KeyValueListTagHelper.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Smart.Design.Razor.TagHelpers.Extensions;
+
+namespace Smart.Design.Razor.TagHelpers.KeyValueList;
+
+/// <summary>
+/// <see cref="ITagHelper" /> implementation of the Smart Design Key Value List.
+/// Documentation can be seen <see href="https://design.smart.coop/development/docs/c-key-value.html">here</see>.
+/// </summary>
+[HtmlTargetElement(TagNames.KeyValueList)]
+public class KeyValueListTagHelper : TagHelper
+{
+    private readonly IKeyValueListHtmlGenerator _keyValueListHtmlGenerator;
+
+    public KeyValueListTagHelper(IKeyValueListHtmlGenerator keyValueListHtmlGenerator)
+    {
+        _keyValueListHtmlGenerator = keyValueListHtmlGenerator;
+    }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        var keyValueList = _keyValueListHtmlGenerator.GenerateKeyValueList();
+
+        output.TagName = keyValueList.TagName;
+        output.TagMode = TagMode.StartTagAndEndTag;
+        output.ClearAndMergeAttributes(keyValueList);
+    }
+}
