d27b870a2d0856edcc2a4f474ad14599faec9825
Nour Jammal <naj@smart.coop>
2022-11-14 12:31:22 +0100

    fix comment

--- src/Smart.Design.Razor/TagHelpers/Header/IHeaderHtmlGenerator.cs
+++ src/Smart.Design.Razor/TagHelpers/Header/IHeaderHtmlGenerator.cs
@@ -10,12 +10,13 @@ public interface IHeaderHtmlGenerator
 {
     /// <summary>
     /// Generate generic header with a <see cref="homePageUrl"/>, string a <see cref="languagesAndLinks"/> dictionary, a <see cref="currentLanguage"/> string,
-    /// a <see cref="userName"/> string,  a <see cref="avatarPath"/> string, <see cref="labelsAndLinks"/> dictionary.
+    /// a <see cref="firstName"/> string, a <see cref="lastName"/> string,  a <see cref="avatarPath"/> string, <see cref="labelsAndLinks"/> dictionary.
     /// </summary>
     /// <param name="homePageUrl"></param>
     /// <param name="languagesAndLinks"></param>
     /// <param name="currentLanguage"></param>
-    /// <param name="userName"></param>
+    /// <param name="firstName"></param>
+    /// <param name="lastName"></param>
     /// <param name="avatarPath"></param>
     /// <param name="labelsAndLinks"></param>
     /// <returns></returns>
