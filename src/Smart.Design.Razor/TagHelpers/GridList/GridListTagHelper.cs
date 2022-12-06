f169aa68f4b00e5669c056071854a263fc9b03ac
Celine Marique <mar@smart.coop>
2022-12-06 16:39:56 +0100

    last

--- src/Smart.Design.Razor/TagHelpers/GridList/GridListTagHelper.cs
+++ src/Smart.Design.Razor/TagHelpers/GridList/GridListTagHelper.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 using Smart.Design.Razor.TagHelpers.Base;
 using Smart.Design.Razor.TagHelpers.Constants;
 
-namespace Smart.Design.Razor.TagHelpers.List;
+namespace Smart.Design.Razor.TagHelpers.GridList;
 
 /// <summary>
 /// <see cref="ITagHelper" /> implementation of a Smart design &lt;grid-list&gt;.
