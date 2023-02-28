ac580f11c585999ecb3c0630799aefdf53b39fed
Joëlle Boucher <joe@smart.coop>
2023-02-28 11:14:57 +0100

    Add Icons component presentation page

---
+++ src/Smart.Design.Showcase.WebApp/Pages/Components/Icons/Icons.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Razor.Showcase.Pages.Components.Icons
+{
+    public class IconsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
