71a3d973a580ddc16214caadd347fa555fdfedd9
marcelpirnay <pirnay.m@gmail.com>
2023-02-28 16:30:29 +0100

    adding colors

---
+++ src/Smart.Design.Showcase.WebApp/Pages/Components/Colors/Colors.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Razor.Showcase.Pages.Components.Colors
+{
+    public class ColorsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
