19be1df110816877af68f0ef32a6309d0be853c6
Joëlle Boucher <joe@smart.coop>
2023-03-01 13:52:51 +0100

    Copy-paste page Colors

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Components/Colors/colors.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Components.Colors
+{
+    public class colorsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
