dec210caf8b448db81960e2913670eda6ef240e4
Lieven Praet <3472148+LievenPraet@users.noreply.github.com>
2023-06-22 11:22:51 +0200

    Add DevExpress page

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Templates/SubscriptionDevExpress.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Templates
+{
+    public class SubscriptionDevExpressModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
