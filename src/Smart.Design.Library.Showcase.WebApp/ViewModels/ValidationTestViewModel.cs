40263c29c319938a01087f4a77a33b811d8dac27
Lieven Praet <3472148+LievenPraet@users.noreply.github.com>
2023-06-22 13:06:59 +0200

    Integrate devexpress controls

---
+++ src/Smart.Design.Library.Showcase.WebApp/ViewModels/ValidationTestViewModel.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Smart.Design.Library.Showcase.ViewModels;
+
+public class ValidationTestViewModel
+{
+    [Required(ErrorMessage = "Do not forget the first name")]
+    [StringLength(20, MinimumLength = 5, ErrorMessage = "First name should be between 5 and 20 characters.")]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string Search { get; set; }
+}
