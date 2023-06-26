166a87e326eef986fe60877860413369ba21bab9
NourJammal <45228497+NourJammal@users.noreply.github.com>
2023-06-26 15:42:02 +0200

    [showcase] Adding the whole css / Js file
    Changing CSS path to the new one generated locally by the scss files
    From here, css can be generated and used inside the proijetc. No DS Needed anymore
    From now, JS is written and compiled from src/js/app.js + modules folder -> into dist/app.js

---
+++ src/Smart.Design.Library.Showcase.WebApp/wwwroot/src/js/app.js
@@ -0,0 +1,20 @@
+import '../scss/app.scss';
+
+/*import './navigation.js';
+
+import './updateBodyClasses.js';
+
+import './barba.js';*/
+
+import "./modules/accordion";
+import "./modules/close-alert";
+import "./modules/close-global-banner";
+import "./modules/copy-btn";
+import "./modules/design-system-website-mobile-menu";
+import "./modules/dialogs";
+import "./modules/input-password.js"
+import "./modules/menu";
+import "./modules/navbar-components";
+import "./modules/popper-popover";
+import "./modules/popper-tooltip";
+import "./modules/tabs";
