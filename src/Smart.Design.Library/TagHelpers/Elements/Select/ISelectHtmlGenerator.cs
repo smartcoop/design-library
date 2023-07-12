ed4ecd6b53481926f88213a5e5d5790037de8372
Nasreddine Azahaf <110824702+azn-smart@users.noreply.github.com>
2023-07-12 22:59:05 +0200

    feature(PAIL-75): Compléter le smart-select

---
+++ src/Smart.Design.Library/TagHelpers/Elements/Select/ISelectHtmlGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Smart.Design.Library.TagHelpers.Elements.Select;
+
+public interface ISelectHtmlGenerator
+{
+    TagBuilder GenerateSelect(
+        ViewContext? viewContext,
+        string? id,
+        string? name,
+        Dictionary<string, string>? items,
+        ModelExpression? @for);
+}
