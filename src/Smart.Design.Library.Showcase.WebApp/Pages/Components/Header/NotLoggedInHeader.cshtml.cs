375c6cec40609919f9becfd112f72aae7fddbbff
Joëlle Boucher <joe@smart.coop>
2023-07-12 13:55:16 +0200

    Add Showcase page for Not Logged In Header

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Components/Header/NotLoggedInHeader.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Components.Header
+{
+    public class NotLoggedInHeaderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
