772208f6f2c2e82a3eedaf7b73d61101255cf013
Joëlle Boucher <joe@smart.coop>
2023-07-13 18:08:19 +0200

    Add Showcase Changelog page

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/ChangeLog.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages
+{
+    public class ChangeLogModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
