b5479a38b9ddb702f4b2e00e527b788264507d00
Nour Jammal <naj@smart.coop>
2023-07-18 16:13:20 +0200

    create unity production page
    Use box tag

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Templates/ProductionUnit.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Templates;
+
+public class ProductionUnitModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
