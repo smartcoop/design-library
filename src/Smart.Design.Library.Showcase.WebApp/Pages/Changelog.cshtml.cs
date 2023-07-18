e57598323f1775a44840a46a108834ff68f415fa
jad001 <45788016+jad001@users.noreply.github.com>
2023-07-18 11:05:25 +0200

    [Showcase] Changelog page (#99)
    # [Showcase]
    
    https://smartjira.atlassian.net/browse/PAIL-64
    
    Add a log page in showcase.

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Changelog.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages
+{
+    public class ChangelogModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
