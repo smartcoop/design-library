57dec9955d1e5114577f009b95e4538626943d54
Nasreddine Azahaf <110824702+Nasreddine-Azahaf@users.noreply.github.com>
2023-08-24 11:36:59 +0200

    hotfix(PAIL-98): [BE - DESIGN] DL - champs téléphone bug (#109)
    # [Library/Showcase]-{PhoneTel}
    
    [JIRA description]([BE - DESIGN] DL - champs "téléphone" bug)
    
    ---------
    
    Co-authored-by: Nasreddine Azahaf <110824702+azn-smart@users.noreply.github.com>

--- src/Smart.Design.Library.Showcase.WebApp/Pages/Templates/Subscription.cshtml.cs
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Templates/Subscription.cshtml.cs
@@ -1,12 +1,64 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Smart.Design.Library.Extensions;
+using Smart.Design.Library.TagHelpers.Elements.Input;
 
-namespace Smart.Design.Library.Showcase.Pages.Templates
+
+namespace Smart.Design.Library.Showcase.Pages.Templates;
+
+
+public class Subscription
 {
-    public class SubscriptionModel : PageModel
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public string Gender { get; set; }
+    public string Language { get; set; }
+    public string Email { get; set; }
+    [PhoneNumberValidation(PhoneType.Landline)]
+    public string? LandLine { get; set; }
+    [PhoneNumberValidation(PhoneType.Mobile)]
+    public string Mobile { get; set; }
+    public DateTime BirthDate{ get; set; }
+    public string BirthTown{ get; set; }
+    public string CivilStatus{ get; set; }
+    public string Nationality{ get; set; }
+    public Address? OfficialAddress{ get; set; }
+    public Address? ShippingAddress{ get; set; }
+    public double Withholding{ get; set; }
+    public string RegisterIdNumber{ get; set; }
+    public string IdCardNumber{ get; set; }
+    public string Iban{ get; set; }
+    public Boolean SubscriptionApproval{ get; set; }
+
+}
+
+public class Address
+{
+    public string? Street { get; set; }
+    public string? Number { get; set; }
+    public string? BoxStreet { get; set; }
+    public string? ZipCode { get; set; }
+    public string? City { get; set; }
+    public string? Country { get; set; }
+}
+
+
+public class SubscriptionModel : PageModel
+{
+    [BindProperty]
+    public Subscription Subscription { get; set; }
+
+    public void OnGet()
     {
-        public void OnGet()
+    }
+    public async Task<ActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
         {
+            ModelState.AddModelError(string.Empty, "error");
         }
+
+        return Page();
     }
 }
