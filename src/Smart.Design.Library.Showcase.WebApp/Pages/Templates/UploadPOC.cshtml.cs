7c1dd613feb688d35f0b2f37efa2114a762cb310
marcelpirnay <pim@smart.coop>
2023-09-19 16:46:54 +0200

    Addinf 6 upload fields

--- src/Smart.Design.Library.Showcase.WebApp/Pages/Templates/UploadPOC.cshtml.cs
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Templates/UploadPOC.cshtml.cs
@@ -1,4 +1,5 @@
 using System.Net.Http.Headers;
+using System.Net.Mail;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -11,52 +12,13 @@ namespace Smart.Design.Library.Showcase.Pages.Templates
 
         }
 
-        public ActionResult OnPostSave(IEnumerable<IFormFile> files)
+        public void OnPost()
         {
-            // The Name of the Upload component is "files"
-            if (files != null)
+            var files  = Request.Form.Files;
+            foreach (var file in files)
             {
-                foreach (var file in files)
-                {
-                    var fileContent = ContentDispositionHeaderValue.Parse(file.ContentDisposition);
-
-                    //// Some browsers send file names with full path.
-                    //// We are only interested in the file name.
-                    var fileName = Path.GetFileName(fileContent?.FileName?.Trim('"'));
-                    //var physicalPath = Path.Combine("App_Data", fileName);
-
-                    //// The files are not actually saved in this demo
-                    ////file.SaveAs(physicalPath);
-                }
+                var fileName = file.FileName;
             }
-
-            // Return an empty string to signify success
-            return Content("");
-        }
-
-        public ActionResult OnPostRemove(string[] fileNames)
-        {
-            // The parameter of the Remove action must be called "fileNames"
-
-            if (fileNames != null)
-            {
-                foreach (var fullName in fileNames)
-                {
-                    var fileName = Path.GetFileName(fullName);
-                    var physicalPath = Path.Combine("App_Data", fileName);
-
-                    // TODO: Verify user permissions
-
-                    if (System.IO.File.Exists(physicalPath))
-                    {
-                        // The files are not actually removed in this demo
-                        // System.IO.File.Delete(physicalPath);
-                    }
-                }
-            }
-
-            // Return an empty string to signify success
-            return Content("");
         }
     }
 }
