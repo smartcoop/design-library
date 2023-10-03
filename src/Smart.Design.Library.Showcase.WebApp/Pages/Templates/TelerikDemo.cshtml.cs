6f2000ed8387947f80f1015e2a55f74dfb9e2495
Lieven Praet <3472148+LievenPraet@users.noreply.github.com>
2023-10-03 14:22:06 +0200

    Fix function call bug

--- src/Smart.Design.Library.Showcase.WebApp/Pages/Templates/TelerikDemo.cshtml.cs
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Templates/TelerikDemo.cshtml.cs
@@ -6,14 +6,41 @@ namespace Smart.Design.Library.Showcase.Pages.Templates;
 
 public class TelerikDemoModel : PageModel
 {
-    public void OnGet()
+    // 6 upload functions are defined here because I coundn't find a way to use the same function for all controls
+    // The parameter name needs to be the same as the control or the files will not be passed to the function
+    // We can further investigate / ask Telerik support if there is a cleaner way of achieving this
+    public ActionResult OnPostSave1(IEnumerable<IFormFile> files1)
     {
+        return OnPostSave(files1);
+    }
 
+    public ActionResult OnPostSave2(IEnumerable<IFormFile> files2)
+    {
+        return OnPostSave(files2);
+    }
+
+    public ActionResult OnPostSave3(IEnumerable<IFormFile> files3)
+    {
+        return OnPostSave(files3);
+    }
+
+    public ActionResult OnPostSave4(IEnumerable<IFormFile> files4)
+    {
+        return OnPostSave(files4);
+    }
+
+    public ActionResult OnPostSave5(IEnumerable<IFormFile> files5)
+    {
+        return OnPostSave(files5);
+    }
+
+    public ActionResult OnPostSave6(IEnumerable<IFormFile> files6)
+    {
+        return OnPostSave(files6);
     }
 
     public ActionResult OnPostSave(IEnumerable<IFormFile> files)
     {
-        // The Name of the Upload component is "files"
         if (files != null)
         {
             foreach (var file in files)
@@ -34,6 +61,7 @@ public class TelerikDemoModel : PageModel
         return Content(string.Empty);
     }
 
+    // We only need 1 remove function because the parameter name is not linked to the control name
     public ActionResult OnPostRemove(string[] fileNames)
     {
         // The parameter of the Remove action must be called "fileNames"
