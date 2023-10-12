7a9ec8adf647a5b190e28f7ba75eef10a68c2adc
David Jadin (JAD) <jad@smart.coop>
2023-10-12 15:39:44 +0200

    [Library/Showcase] feature(PAIL-159): TT-DL · copier ~20 composants html dans Showcase

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Components/03_cat_ds/FlexboxLayout/FlexboxLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Components._03_cat_ds.FlexboxLayout
+{
+    public class FlexboxLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Components/03_cat_ds/Tabs/Tabs.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Components.Tabs;
+
+public class TabsModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
