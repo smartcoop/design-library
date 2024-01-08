f0882bfd75e9ef3a66eb685430433a564519939a
Nasreddine Azahaf <110824702+azn-smart@users.noreply.github.com>
2024-01-08 22:46:08 +0100

    feature(TT-160): TT-DL · [BE - DESIGN] copier ~20 composants html dans Showcase (Navbar, pagination, panel)

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Components/03_cat_ds/Navbar/Navbar.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Components._03_cat_ds.Navbar;
+
+public class Navbar : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Components/03_cat_ds/Panel/panel.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Components._03_cat_ds.Panel;
+
+public class panel : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
