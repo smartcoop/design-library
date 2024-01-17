778a655acf25269f8e28a264d0d39e5146d58755
Mélissa Charlier <melissa.charlier@smart.coop>
2024-01-17 17:28:41 +0100

    Add IconList page

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Components/03_cat_ds/IconList/IconList.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Components.IconList;
+
+public class IconList : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
