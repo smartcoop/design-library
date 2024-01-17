3fc39753598e79909b7e68c40b5e6db959d4fb8f
Mélissa Charlier <melissa.charlier@smart.coop>
2024-01-17 17:27:13 +0100

    Add FilterPill page

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Components/03_cat_ds/FilterPill/FilterPill.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Components.FilterPill
+{
+    public class FilterPillModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
