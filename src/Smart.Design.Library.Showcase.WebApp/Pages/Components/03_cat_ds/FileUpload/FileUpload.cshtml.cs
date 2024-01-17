cda872ed92a17e04071fa2514266edcca8a0853d
Mélissa Charlier <melissa.charlier@smart.coop>
2024-01-17 17:26:54 +0100

    Add FileUpload page

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Components/03_cat_ds/FileUpload/FileUpload.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Components._03_cat_ds.FileUpload
+{
+    public class FileUploadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
