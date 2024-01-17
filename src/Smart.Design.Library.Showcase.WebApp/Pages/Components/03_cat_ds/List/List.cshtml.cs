7469b07e625156bdd0e12a015302e4e3008474e7
Mélissa Charlier <melissa.charlier@smart.coop>
2024-01-17 17:29:44 +0100

    Add List page

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Components/03_cat_ds/List/List.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Components._03_cat_ds.List
+{
+    public class ListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
