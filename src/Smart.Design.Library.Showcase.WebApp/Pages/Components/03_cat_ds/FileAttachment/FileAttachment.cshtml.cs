dd4bf2281d37ee28ae2359ed7ac49cefd834e692
Mélissa Charlier <melissa.charlier@smart.coop>
2024-01-17 17:26:26 +0100

    Add FileAttachment page

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Components/03_cat_ds/FileAttachment/FileAttachment.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Components._03_cat_ds.FileAttachment
+{
+    public class FileAttachmentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
