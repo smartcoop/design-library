8cacf735ad8851ead9bfe84b9183bc33ffc227b8
marcelpirnay <pim@smart.coop>
2024-01-23 11:27:39 +0100

    CSS for burger menu + start js

---
+++ src/Smart.Design.Library.Showcase.WebApp/wwwroot/SmartLib/js/index.es5.js
@@ -0,0 +1,26 @@
+﻿"use strict";
+
+require("./mono-modules/accordion");
+
+require("./mono-modules/close-alert");
+
+require("./mono-modules/close-global-banner");
+
+require("./mono-modules/copy-btn");
+
+require("./mono-modules/design-system-website-mobile-menu");
+
+require("./mono-modules/dialogs");
+
+require("./mono-modules/input-password.js");
+
+require("./mono-modules/menu");
+
+require("./mono-modules/navbar-components");
+
+require("./mono-modules/popper-popover");
+
+require("./mono-modules/popper-tooltip");
+
+require("./mono-modules/tabs");
+
