e0c1d0285b2e6eec7be44853c36b337af021afb8
marcelpirnay <pim@smart.coop>
2024-01-23 10:31:27 +0100

    Creating Default Template page and dividing shared elements (header, side-menu, main-title)

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Templates/DefaultTemplate.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Templates;
+
+public class DefaultTemplateModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
