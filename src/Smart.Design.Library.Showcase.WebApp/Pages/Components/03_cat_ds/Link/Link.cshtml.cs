97a1ca53fbb9676469ead6afcd4fca9fc79868f3
ChMelissa <41069782+ChMelissa@users.noreply.github.com>
2024-01-30 09:51:28 +0100

    Feature/tt 160 move monos components into dl paillette (#131)
    # [Design Library - Showcase]
    
    
    [https://smartjira.atlassian.net/browse/TT-160](https://smartjira.atlassian.net/browse/TT-160)
    
    Add 17 HTML components and reorder the component list. 
    
    🚀 Design Library
    
    ---------
    
    Co-authored-by: Mélissa Charlier <melissa.charlier@smart.coop>

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Components/03_cat_ds/Link/Link.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Components._03_cat_ds.Link
+{
+    public class LinkModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- src/Smart.Design.Library.Showcase.WebApp/Pages/Components/03_cat_ds/Navbar/Navbar.cshtml.cs
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Components/03_cat_ds/Navbar/Navbar.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Smart.Design.Library.Showcase.Pages.Components._03_cat_ds.Navbar;
 
