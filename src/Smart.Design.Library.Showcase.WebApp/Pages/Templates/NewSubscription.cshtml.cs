0178d91e07353ee17631dea006b0681be431cb37
Joëlle Boucher <joe@smart.coop>
2024-02-21 17:24:16 +0100

    First Try

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Templates/NewSubscription.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Templates;
+
+public class NewSubscriptionModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
