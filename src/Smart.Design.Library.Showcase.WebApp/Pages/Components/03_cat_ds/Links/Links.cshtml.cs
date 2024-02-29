10b95077d4ba32429a29189fa5ecca83cef5d0cf
marcelpirnay <pim@smart.coop>
2024-02-29 16:14:43 +0100

    Buttons, Button toolbar and Links

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Components/03_cat_ds/Links/Links.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Components.Links;
+
+public class Links : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
