2f244ac695b5c24c8550646756363f96d76a76ee
Joëlle <41791847+joe-coop@users.noreply.github.com>
2024-03-05 13:52:19 +0100

    Feature(TT-220) : Ajouter les eléments pour pouvoir utiliser Telerik + theme builder (#138)
    # [Showcase]-{TT-220} Add elements for Telerik
    
    [JIRA description](https://smartjira.atlassian.net/browse/TT-220)
    
    - add licence Telerik licence
    - add  js for Telerik
    - add css for themeBuilder
    - add package Telerik
    - fix favicon problem

--- src/Smart.Design.Library.Showcase.WebApp/Program.cs
+++ src/Smart.Design.Library.Showcase.WebApp/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddSmartDesign();
-
+builder.Services.AddKendo();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
