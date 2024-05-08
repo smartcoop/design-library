8afdbff0b5d80f76fb7bc87ea485ff02cf45b587
marcelpirnay <pim@smart.coop>
2024-05-08 12:58:33 +0200

    New banner 2024

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Templates/DashboardWithCommunication.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Templates;
+
+public class DashboardWithCommunicationModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
