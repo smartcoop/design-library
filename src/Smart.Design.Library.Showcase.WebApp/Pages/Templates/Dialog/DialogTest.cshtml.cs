c3a7509837e77797c835119c96ab47e1a42a93ad
jad <jad@smart.coop>
2024-06-06 15:12:11 +0200

    Create razor page

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Templates/Dialog/DialogTest.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Templates.Dialog
+{
+    public class DialogTestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
